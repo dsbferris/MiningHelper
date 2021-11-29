bae66c219cd484bf629d57440084d71c03bdfc98
Ferris Schwanebeck <ferris.s@aol.de>
2021-11-29 15:01:50 +0100

    Projektdateien hinzufügen.

---
+++ MiningHelper/Helper/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MiningHelper.Helper
+{
+    public static class Settings
+    {
+        private static object? GetSettings(string name)
+        {
+            if (!Application.Current.Properties.Contains(name)) return null;
+            return Application.Current.Properties[name];
+        }
+
+        private static void SetSettings(string name, object value)
+        {
+            Application.Current.Properties[name] = value;
+        }
+
+        private static string _adminrequired = "AdminRequired";
+        public static bool AdminRequired { get { return (bool?)GetSettings(_adminrequired) ?? false; } set { SetSettings(_adminrequired, value); } }
+
+
+
+    }
+}
---
+++ MiningHelper/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Settings = MiningHelper.Helper.Settings;
+
+namespace MiningHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            var args = Environment.GetCommandLineArgs();
+            if (args.Contains("-run")){
+                MessageBox.Show("Run");
+            }
+
+            
+
+            InitializeComponent();
+        }
+
+        private void Label_Home_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Label_PowerPlan_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Label_MinerConfig_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Label_LogAnalyser_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
